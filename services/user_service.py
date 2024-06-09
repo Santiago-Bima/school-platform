fd1220e7b8c33a04941ecdd6fe5f9c5389833771
Santiago-Bima <santiago.bima@gmail.com>
2024-06-09 19:59:17 -0300

    Adding user service

---
+++ services/user_service.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractclassmethod
+
+class UserService(ABC):
+  @abstractclassmethod
+  def insert_user(self, user):
+    pass
+  
+  @abstractclassmethod
+  def get_by_username(self, username):
+    pass
+  
+  @abstractclassmethod
+  def get_all(self):
+    pass
+  
+  @abstractclassmethod
+  def user_validation(self, username, password):
+    pass
+  
+  @abstractclassmethod
+  def update(self, user, id):
+    pass
+  
+  @abstractclassmethod
+  def delete(self, id):
+    pass
