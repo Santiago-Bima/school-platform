fa94fbffb99b710115226f9e7c943820ef2bb007
Santiago-Bima <santiago.bima@gmail.com>
2024-06-09 19:55:42 -0300

    Adding the subjects models

---
+++ models/grade.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Grade(Enum):
+    FIRST = 1
+    SECOND = 2
+    THIRD = 3
---
+++ models/subject.py
@@ -0,0 +1,65 @@
+from grade import Grade
+
+class Subject:
+  def __init__(self, id=0, name='', price=0, final_date='', grade=None, suscriptions=None):
+    self._id = id
+    self._name = name
+    self._price = price
+    self._final_date = final_date
+    self._grade = grade if grade is not None else Grade()
+    self._suscriptions = suscriptions if suscriptions is not None else []
+
+  @property
+  def id(self):
+      return self._id
+      
+  @id.setter
+  def id(self, id):
+      self._id = id
+
+  @property
+  def name(self):
+      return self._name
+
+  @name.setter
+  def name(self, name):
+      self._name = name
+
+  @property
+  def price(self):
+      return self._price
+
+  @price.setter
+  def price(self, price):
+      self._price = price
+
+  @property
+  def final_date(self):
+      return self._final_date
+
+  @final_date.setter
+  def final_date(self, final_date):
+      self._final_date = final_date
+
+  @property
+  def grade(self):
+      return self._grade
+
+  @grade.setter
+  def grade(self, grade):
+      self._grade = grade
+      
+  @property
+  def suscriptions(self):
+      return self._suscriptions
+
+  @suscriptions.setter
+  def suscriptions(self, suscriptions):
+      self._suscriptions = suscriptions
+      
+  def add_suscription(self, suscription):
+    self._suscriptions.append(suscription)
+  
+  def remove_suscription(self, suscription):
+    if suscription in self._suscriptions:
+      self._suscriptions.remove(suscription)
