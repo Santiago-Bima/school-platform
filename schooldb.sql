71c1f4b710d136e3c32baf4c4d18f8dd4e0aea61
Santiago-Bima <santiago.bima@gmail.com>
2024-06-24 17:03:08 -0300

    updating database script

--- schooldb.sql
+++ schooldb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 07-06-2024 a las 23:11:58
+-- Tiempo de generación: 24-06-2024 a las 21:53:38
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -48,11 +48,19 @@ INSERT INTO `grades` (`id`, `grade`) VALUES
 --
 
 CREATE TABLE `marks` (
-  `id_subscription` int(11) NOT NULL,
-  `mark` int(11) NOT NULL,
   `id_mark` int(11) NOT NULL,
-  `date` date NOT NULL
+  `mark` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `id_subscription` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `marks`
+--
+
+INSERT INTO `marks` (`id_mark`, `mark`, `date`, `id_subscription`) VALUES
+(8, 9, '2024-06-15', 2),
+(9, 7, '2024-08-20', 2);
 
 -- --------------------------------------------------------
 
@@ -64,9 +72,21 @@ CREATE TABLE `subjects` (
   `id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL,
   `price` int(11) NOT NULL,
+  `begining_date` date NOT NULL,
   `final_date` date NOT NULL,
   `grade` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `name`, `price`, `begining_date`, `final_date`, `grade`) VALUES
+(2, 'algebra', 4000, '2024-08-01', '2024-12-10', 2),
+(3, 'math', 3500, '2024-06-30', '2024-12-10', 2),
+(4, 'math', 3500, '2024-06-15', '2024-12-10', 1),
+(5, 'dabd', 5000, '2024-04-10', '2024-12-10', 1),
+(6, 'dabd', 5000, '2024-04-10', '2024-12-08', 2);
 
 -- --------------------------------------------------------
 
@@ -75,11 +95,25 @@ CREATE TABLE `subjects` (
 --
 
 CREATE TABLE `subscriptions` (
+  `id_subscription` int(11) NOT NULL,
   `id_subject` int(11) NOT NULL,
-  `id_user` varchar(30) NOT NULL,
-  `inscription_date` date NOT NULL,
-  `id_subscription` int(11) NOT NULL
+  `id_user` int(11) NOT NULL,
+  `inscription_date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `subscriptions`
+--
+
+INSERT INTO `subscriptions` (`id_subscription`, `id_subject`, `id_user`, `inscription_date`) VALUES
+(3, 2, 7, '2024-06-12'),
+(2, 2, 11, '2024-06-14'),
+(9, 2, 12, '2024-06-24'),
+(6, 3, 11, '2024-06-24'),
+(10, 3, 12, '2024-06-24'),
+(11, 4, 14, '2024-06-24'),
+(12, 5, 14, '2024-06-24'),
+(7, 6, 11, '2024-06-24');
 
 -- --------------------------------------------------------
 
@@ -88,10 +122,22 @@ CREATE TABLE `subscriptions` (
 --
 
 CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `username` varchar(20) NOT NULL,
-  `password` int(11) NOT NULL,
+  `password` varchar(10) NOT NULL,
   `id_type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `id_type`) VALUES
+(7, '', '', 2),
+(10, 'prueba', '123456', 2),
+(11, 'julian2', '123456', 1),
+(12, 'mariano', '123456', 1),
+(14, 'agustin', '123456', 1);
 
 -- --------------------------------------------------------
 
@@ -126,13 +172,15 @@ ALTER TABLE `grades`
 -- Indices de la tabla `marks`
 --
 ALTER TABLE `marks`
-  ADD PRIMARY KEY (`id_mark`);
+  ADD PRIMARY KEY (`id_mark`),
+  ADD KEY `fk_subscription` (`id_subscription`);
 
 --
 -- Indices de la tabla `subjects`
 --
 ALTER TABLE `subjects`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `index_name_grade` (`name`,`grade`) USING BTREE,
   ADD KEY `fk_grade` (`grade`);
 
 --
@@ -140,14 +188,15 @@ ALTER TABLE `subjects`
 --
 ALTER TABLE `subscriptions`
   ADD PRIMARY KEY (`id_subject`,`id_user`),
-  ADD UNIQUE KEY `id_subscription` (`id_subscription`),
+  ADD UNIQUE KEY `id_subscription` (`id_subscription`) USING BTREE,
   ADD KEY `fk_user` (`id_user`);
 
 --
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`username`),
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
   ADD KEY `fk_user_types` (`id_type`);
 
 --
@@ -170,19 +219,25 @@ ALTER TABLE `grades`
 -- AUTO_INCREMENT de la tabla `marks`
 --
 ALTER TABLE `marks`
-  MODIFY `id_mark` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_mark` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `subscriptions`
 --
 ALTER TABLE `subscriptions`
-  MODIFY `id_subscription` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_subscription` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `user_types`
@@ -195,23 +250,29 @@ ALTER TABLE `user_types`
 --
 
 --
+-- Filtros para la tabla `marks`
+--
+ALTER TABLE `marks`
+  ADD CONSTRAINT `fk_subscription` FOREIGN KEY (`id_subscription`) REFERENCES `subscriptions` (`id_subscription`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `subjects`
 --
 ALTER TABLE `subjects`
-  ADD CONSTRAINT `fk_grade` FOREIGN KEY (`grade`) REFERENCES `grades` (`id`);
+  ADD CONSTRAINT `fk_grade` FOREIGN KEY (`grade`) REFERENCES `grades` (`id`) ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `subscriptions`
 --
 ALTER TABLE `subscriptions`
-  ADD CONSTRAINT `fk_student` FOREIGN KEY (`id_user`) REFERENCES `users` (`username`),
-  ADD CONSTRAINT `fk_subject` FOREIGN KEY (`id_subject`) REFERENCES `subjects` (`id`);
+  ADD CONSTRAINT `fk_subject` FOREIGN KEY (`id_subject`) REFERENCES `subjects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `fk_user_types` FOREIGN KEY (`id_type`) REFERENCES `user_types` (`id`);
+  ADD CONSTRAINT `fk_user_types` FOREIGN KEY (`id_type`) REFERENCES `user_types` (`id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
